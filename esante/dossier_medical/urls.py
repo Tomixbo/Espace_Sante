7ef2263216d07e750705b24e1d6fe889f3ea4536
Tomixbo <tomixbo@gmail.com>
2024-10-19 13:34:34 +0300

    Bouton, Modal pour ajouter historique consultation

--- esante/dossier_medical/urls.py
+++ esante/dossier_medical/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('dossier-medical/<int:member_id>/', views.get_dossier_medical, name='get_dossier_medical'),
     path('list-members/', views.list_members, name='list_members'),
+    path('dossier-medical/<int:member_id>/add-consultation/', views.add_consultation, name='add_consultation'),
 ]
--- esante/dossier_medical/views.py
+++ esante/dossier_medical/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from accounts.models import CustomUser
-from .models import MedicalRecord
+from .models import MedicalRecord, ConsultationHistory
 from django.contrib.auth.decorators import login_required
 from accounts.views import medecin_required  # Assurez-vous d'importer votre décorateur
-
-
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
 def get_dossier_medical(request, member_id):
 
@@ -29,3 +29,32 @@ def list_members(request):
         'members': members,
     }
     return render(request, 'dossier_medical/list_members.html', context)
+
+@login_required
+@medecin_required
+@require_POST
+def add_consultation(request, member_id):
+    membre = get_object_or_404(CustomUser, pk=member_id, user_type='membre')
+    medical_record, created = MedicalRecord.objects.get_or_create(member=membre)
+
+    # Récupérer les données du formulaire
+    date = request.POST.get('date')
+    establishment = request.POST.get('establishment')
+    symptoms = request.POST.get('symptoms')
+    diagnosis = request.POST.get('diagnosis')
+    prescriptions = request.POST.get('prescriptions', '')
+    treatments = request.POST.get('treatments', '')
+
+    # Créer une nouvelle consultation
+    ConsultationHistory.objects.create(
+        medical_record=medical_record,
+        date=date,
+        doctor=request.user,
+        establishment=establishment,
+        symptoms=symptoms,
+        diagnosis=diagnosis,
+        prescriptions=prescriptions,
+        treatments=treatments,
+    )
+
+    return JsonResponse({'message': 'Consultation ajoutée avec succès.'})
